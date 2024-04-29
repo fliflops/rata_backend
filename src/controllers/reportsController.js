bf8dbd2b83a115d4b8b0670d9cc903ee665e8276
fliflops <carlo.vincnt@gmail.com>
2024-04-29 16:41:09 +0800

    feature: first commit

--- src/controllers/reportsController.js
+++ src/controllers/reportsController.js
@@ -161,3 +161,24 @@ exports.downloadReport = async(req,res,next) => {
 exports.pod = async (req,res,next) => {
     
 }
+
+exports.reverseLogistics = async(req,res,next) => {
+    try{
+        const draftBill = await reportService.getDraftBill({
+            service_type:'2007',
+            updatedAt: {
+                [sequelize.Op.between]:['2024-01-01 00:00:00', '2024-01-31 00:00:00']
+                //[sequelize.Op.between]:[filter.from,filter.to]
+            },
+        })
+
+        const ascii = await asciiService.getSalesOrder(draftBill.map(item => item.draft_bill_no))
+
+        res.status(200).json({
+            data:draftBill.filter(item => ascii.map(a => a.SO_CODE).includes(item.draft_bill_no)),
+        });
+    }
+    catch(e){
+        next(e)
+    }
+}
