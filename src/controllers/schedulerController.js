4cf4d5700c8979e96adcf9454b92f20537d9b7f1
fliflops <carlo.vincnt@gmail.com>
2024-11-12 18:05:52 +0800

    revert back to 7 days

--- src/controllers/schedulerController.js
+++ src/controllers/schedulerController.js
@@ -127,7 +127,7 @@ exports.postManualTrigger = async(req,res,next) => {
         if(['RATA_DRAFT_BILL_BUY_RANGED', 'RATA_DRAFT_BILL_SELL_RANGED'].includes(id)){
             await Queue[id].add({
                 isRepeatable: false,
-                from: moment(date).subtract(30,'days').format('YYYY-MM-DD'), 
+                from: moment(date).subtract(7,'days').format('YYYY-MM-DD'), 
                 to: moment(date).format('YYYY-MM-DD')
             },
             {
